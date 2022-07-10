5e6b1135c478b870da7b79b5bdec0ab2ca570b7f
Miyukiichan <sam.matthews19638@gmail.com>
2022-07-10 11:46:46 +0100

    - Moving stacks of cards in play to other stacks

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # solipair
+
 Multiplayer competative solitaire game
+
+## Todo
+
+### Must
+
+- Discard to oponent's player's discard pile
+	- Rule: if (card being placed needs to be 1 above/below (wrapping supported) and be the same suite) OR (no cards in discar pile) then card placed on oponent's discard pile
+	- Can only do this for non-ace card stack - drawn cards and cards in main play are allowed
+- Ace piles
+	- Can start any pile with any ace
+	- Ensure we can take cards from the board and place on ace piles
+		- Only allow draging for 1st and last card
+		- If dragging last card - don't move enture stack
+			- Improve dragging algorithm to only perform drag operations on all cards going up the stack from current card pos - makes things generic
+		- If dragging last card - don't allow movement to the other stacks play stacks - only the ace stacks
+- Tidy up UI
+- Add card images to panels - possibly replace panels for image controls to make that easier
+
+### Maybe
+
+- More than 2 players
+- Use something other than winforms - maybe convert to Blazor/Unity/Maui
--- Solipair/Form1.cs
+++ Solipair/Form1.cs
@@ -12,6 +12,7 @@ namespace Solipair {
         public Stack<GUICard> skGUI3;
         public Stack<GUICard> skGUI4; 
         private Point _mouseDownLocation;
+        private Panel? _startingPanel;
         private void InitCurrent(out Panel p, out Label l) {
             p = new Panel();
             l = new Label();
@@ -28,8 +29,8 @@ namespace Solipair {
             p.Width = CardWidth;
         }
         private void InitCurrentEvents(Control c) {
-            c.MouseMove += CurrentMoved;
-            c.MouseDown += CurrentMouseDown;
+            c.MouseMove += CardMoved;
+            c.MouseDown += CardMouseDown;
             c.MouseUp += CurrentMouseUp;
         }
 
@@ -55,56 +56,150 @@ namespace Solipair {
             Reset();
         }
 
-        private void CurrentMouseDown(object? sender, MouseEventArgs e) {
+        private void CardMouseDown(object? sender, MouseEventArgs e) {
             _mouseDownLocation = e.Location;
+            Panel? p = GetSenderPanel(sender);
+            if (p == null) return;
+            p = p.Parent as Panel;
+            if (p == null) return;
+            _startingPanel = p;
         }
-
-        private void CurrentMoved(object? sender, MouseEventArgs e) {
-            if (e.Button != MouseButtons.Left) return;
+        private Panel? GetSenderPanel(object? sender) {
             Panel? p = sender as Panel;
             //Assume event is coming from the label if not the panel - we can get the panel from the label parent
             if (p == null) {
                 Label? l = sender as Label;
-                if (l == null) return;
+                if (l == null) return null;
                 p = l.Parent as Panel;
-                if (p == null) return;
+                if (p == null) return null;
             }
-            this.Controls.SetChildIndex(p, 0);
+            return p;
+        }
+
+        private void CardMoved(object? sender, MouseEventArgs e) {
+            if (e.Button != MouseButtons.Left) return;
+            Panel? p = GetSenderPanel(sender);
+            if (p == null) return;
+            //Make it so cards in play get moved out from the board panels so we can move them around the whole screen
+            p.Parent = this;
+            p.Parent.Controls.SetChildIndex(p, 0);
             p.Location = new Point(e.X + p.Left - _mouseDownLocation.X, e.Y + p.Top - _mouseDownLocation.Y);
+        }
+        private void RemovePlayCardEvents(Control c) {
+            c.MouseDown -= CardMouseDown;
+            c.MouseUp -= CardMouseUp;
+            c.MouseMove -= PlayCardMoved;
+        }
+        private void AddPlayCardEvents(Control c) {
+            c.MouseDown += CardMouseDown;
+            c.MouseUp += CardMouseUp;
+            c.MouseMove += PlayCardMoved;
+        }
+        private void ResetStack(Stack<GUICard> sk, Panel pBase) {
+            for (int i = sk.Count() - 1; i >= 0; i--) {
+                var element = sk.ElementAt(i);
+                Panel panel = element.Panel;
+                panel.Parent = pBase;
+                panel.Location = new Point(0, GetCardPos(skGUI1, sk.Count() - 1 - i));
+                pBase.Controls.SetChildIndex(panel, i);
+                RemovePlayCardEvents(panel);
+                RemovePlayCardEvents(element.Label);
+            }
+            if (!sk.Any()) return;
+            var e = sk.ElementAt(sk.Count() - 1);
+            AddPlayCardEvents(e.Panel);
+            AddPlayCardEvents(e.Label);
         }
         private void ResetCardPos() {
             pP1Current.Location = new Point(pP1Discard.Right + 20, pP1Discard.Top);
             pP2Current.Location = new Point(pP2Discard.Left - pP2Current.Width - 20, pP2Discard.Top);
+            ResetStack(skGUI1, pCard1);
+            ResetStack(skGUI2, pCard2);
+            ResetStack(skGUI3, pCard3);
+            ResetStack(skGUI4, pCard4);
         }
         private void CurrentMouseUp(object? sender, MouseEventArgs e) {
-            Panel? p = sender as Panel;
-            // Assume event is coming from the label if not the panel - we can get the panel from the label parent
-            if (p == null) {
-                Label? l = sender as Label;
-                if (l == null) return;
-                p = l.Parent as Panel;
-                if (p == null) return;
-            }
-            var c = new Point(p.Left + (p.Width / 2), p.Top + (p.Height / 2));
+            Panel? p = GetSenderPanel(sender);
+            if (p == null) return;
             var disc = pP1Discard;
             if (p == pP2Current)
                 disc = pP2Discard;
-            if (c.X < disc.Right && c.X > disc.Left && c.Y > disc.Top && c.Y < disc.Bottom) {
+            if (Collided(p, disc)) {
                 if (p == pP1Current)
                     pP1Discard_MouseClick(sender, e);
                 else
                     pP2Discard_MouseClick(sender, e);
             }
-            else if (c.X < pCard1.Right && c.X > pCard1.Left && c.Y > pCard1.Top && c.Y < pCard1.Bottom)
+            else if (Collided(p, pCard1))
                 pCard1_MouseClick(sender, e);
-            else if (c.X < pCard2.Right && c.X > pCard2.Left && c.Y > pCard2.Top && c.Y < pCard2.Bottom)
+            else if (Collided(p, pCard2))
                 pCard2_MouseClick(sender, e);
-            else if (c.X < pCard3.Right && c.X > pCard3.Left && c.Y > pCard3.Top && c.Y < pCard3.Bottom)
+            else if (Collided(p, pCard3))
                 pCard3_MouseClick(sender, e);
-            else if (c.X < pCard4.Right && c.X > pCard4.Left && c.Y > pCard4.Top && c.Y < pCard4.Bottom)
+            else if (Collided(p, pCard4))
                 pCard4_MouseClick(sender, e);
             else
                 ResetCardPos();
+        }
+
+        private void CardMouseUp(object? sender, MouseEventArgs e) {
+            if (_startingPanel == null) return;
+            Panel? p = GetSenderPanel(sender);
+            if (p == null) return;
+            Stack<Card>? skFrom = null;
+            Stack<GUICard>? skGUIFrom = null;
+            if (_startingPanel == pCard1) {
+                skFrom = controller.sk1;
+                skGUIFrom = skGUI1;
+            }
+            else if (_startingPanel == pCard2) {
+                skFrom = controller.sk2;
+                skGUIFrom = skGUI2;
+            }
+            else if (_startingPanel == pCard3) {
+                skFrom = controller.sk3;
+                skGUIFrom = skGUI3;
+            }
+            else if (_startingPanel == pCard4) {
+                skFrom = controller.sk4;
+                skGUIFrom = skGUI4;
+            }
+            if (skFrom == null || skGUIFrom == null) return;
+            bool reset = true;
+            if (_startingPanel != pCard1 && Collided(p, pCard1)) {
+                reset = !MoveStack(skFrom, skGUIFrom, _startingPanel, controller.sk1, skGUI1, pCard1);
+            }
+            else if (_startingPanel != pCard2 && Collided(p, pCard2)) {
+                reset = !MoveStack(skFrom, skGUIFrom, _startingPanel, controller.sk2, skGUI2, pCard2);
+            }
+            else if (_startingPanel != pCard3 && Collided(p, pCard3)) {
+                reset = !MoveStack(skFrom, skGUIFrom, _startingPanel, controller.sk3, skGUI3, pCard3);
+            }
+            else if (_startingPanel != pCard4 && Collided(p, pCard4)) {
+                reset = !MoveStack(skFrom, skGUIFrom, _startingPanel, controller.sk4, skGUI4, pCard4);
+            }
+            if (reset) {
+                //Workaround for visual bug - single reset does not stack the cards properly visually - just call the function twice
+                ResetCardPos();
+                ResetCardPos();
+            }
+        }
+        private bool MoveStack(Stack<Card> skFrom, Stack<GUICard> skGUIFrom, Panel pFrom, Stack<Card> skTo, Stack<GUICard> skGUITo, Panel pTo) {
+            if (!controller.MergeStacks(skFrom, skTo)) return false;
+            Stack<GUICard> tmp = new Stack<GUICard>();
+            while (skGUIFrom.Any()) 
+                tmp.Push(skGUIFrom.Pop());
+            while (tmp.Any()) 
+                skGUITo.Push(tmp.Pop());
+            //Workaround for visual bug - single reset does not stack the cards properly visually - just call the function twice
+            ResetStack(skGUITo, pTo);
+            ResetStack(skGUITo, pTo);
+            return true;
+        }
+
+        private bool Collided(Control p1, Control p2) {
+            var c = new Point(p1.Left + (p1.Width / 2), p1.Top + (p1.Height / 2));
+            return (c.X < p2.Right && c.X > p2.Left && c.Y > p2.Top && c.Y < p2.Bottom);
         }
 
         private void ResetGUIStack(Stack<GUICard> sk, Panel p) {
@@ -173,6 +268,11 @@ namespace Solipair {
             if (c == null) return;
             c.Panel.MouseClick += pCard4_MouseClick;
         }
+        private int GetCardPos(Stack<GUICard> skGUI, int index = -1) {
+            if (index < 0)
+                index = skGUI.Count() - 1;
+            return index * 25;
+        }
         private GUICard? AddCard(Stack<Card>? sk, Stack<GUICard>? skGUI, Panel p) {
             if (sk == null || skGUI == null) return null;
             Card? current = controller.ActivePlayer.Current;
@@ -181,12 +281,38 @@ namespace Solipair {
             if (!added) return null;
             var c = new GUICard(current);
             skGUI.Push(c);
-            int offset = (skGUI.Count() - 1) * 25;
             c.Panel.Parent = p;
-            c.Panel.Top = offset;
+            c.Panel.Top = GetCardPos(skGUI);
             p.Controls.SetChildIndex(c.Panel, 0);
+            if (skGUI.Count == 1) {
+                //c.Panel.MouseMove += CardMoved;
+                c.Panel.MouseMove += PlayCardMoved;
+                c.Panel.MouseDown += CardMouseDown;
+                c.Panel.MouseUp += CardMouseUp;
+                //c.Label.MouseMove += CardMoved;
+                c.Label.MouseMove += PlayCardMoved;
+                c.Label.MouseDown += CardMouseDown;
+                c.Label.MouseUp += CardMouseUp;
+            }
             SwitchPlayer();
             return c;
+        }
+
+        private void PlayCardMoved(object? sender, MouseEventArgs e) {
+            if (e.Button != MouseButtons.Left) return;
+            Stack<GUICard>? gc = null;
+            if (_startingPanel == pCard1)
+                gc = skGUI1;
+            else if (_startingPanel == pCard2)
+                gc = skGUI2;
+            else if (_startingPanel == pCard3)
+                gc = skGUI3;
+            else if (_startingPanel == pCard4)
+                gc = skGUI4;
+            if (gc == null) return;
+            foreach (var c in gc) {
+                CardMoved(c.Panel, e);
+            }
         }
 
         private void pP1Discard_MouseClick(object? sender, MouseEventArgs e) {
@@ -309,14 +435,27 @@ namespace Solipair {
         public string PlayerTurn() {
             return $"{ActivePlayer.Name}'s turn";
         }
-        public bool AddToStack(Stack<Card> sk, Card c) {
-            if (ActivePlayer.Current != c) return false;
+        public bool CanAddToStack(Stack<Card> sk, Card c, bool checkCurrent = true) {
+            if (checkCurrent && ActivePlayer.Current != c) return false;
             if (sk.Any()) {
                 if (sk.Peek().GetColour() == c.GetColour()) return false;
                 if ((sk.Peek().Value - c.Value) != 1) return false;
             }
+            return true;
+        }
+        public bool AddToStack(Stack<Card> sk, Card c) {
+            if (!CanAddToStack(sk, c)) return false;
             sk.Push(c);
             ActivePlayer.Current = null;
+            return true;
+        }
+        public bool MergeStacks(Stack<Card> from, Stack<Card> to) {
+            if (!CanAddToStack(to, from.Last(), false)) return false;
+            Stack<Card> tmp = new Stack<Card>();
+            while (from.Any()) 
+                tmp.Push(from.Pop());
+            while (tmp.Any())
+                to.Push(tmp.Pop());
             return true;
         }
         public void SwitchPlayer() {
